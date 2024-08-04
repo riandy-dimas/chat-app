b6403dfd0b52378f165a9d4d681422e59f683097
Riandy Dimas <riandy.dimas@live.com>
2024-08-04 13:56:17 +0700

    fix: chat width (#9)

--- components/client/message-list.tsx
+++ components/client/message-list.tsx
@@ -55,7 +55,7 @@ const MessageList = () => {
   return (
     <div className="grid h-dvh grid-rows-[1fr_0fr] gap-2 bg-blue-200 p-3 pt-[4.5rem] sm:h-full sm:pt-3">
       <div className="flex h-[664px] overflow-auto rounded-md border-none bg-blue-100 p-4 shadow-inner sm:border">
-        <ScrollArea className="flex h-full flex-col gap-3">
+        <ScrollArea className="flex h-full w-full flex-col gap-3">
           {messages.map(
             ({ id: msgId, message, timestamp, email, name, userId }) => {
               return (
